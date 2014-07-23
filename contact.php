0011a191446928c8165040a3ed23a4d7b4c809c1
Nahakiole <robin.glauser@gmail.com>
2014-07-23 14:18:07 +0200

    Added empty check for mail and message.

--- contact.php
+++ contact.php
@@ -10,6 +10,9 @@ $app = new Silex\Application();
 $app->post('/send', function (Request $request) use ($app) {
         $message = $request->get('textarea');
         $mail = $request->get('email');
+        if (empty($mail) || empty($message)){
+            return new Response('This is bad!', 201);
+        }
         mail('robin.glauser@gmail.com', 'Feedback from '.$mail, $message);
         return new Response('Thank you for your feedback!', 201);
 });
