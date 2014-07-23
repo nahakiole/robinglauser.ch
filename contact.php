50eefe2630e514c4281bae2a624f68f5c5ee8e01
Nahakiole <robin.glauser@gmail.com>
2014-07-23 13:40:34 +0200

    Added form handler and silex framework

--- contact.php
+++ contact.php
@@ -8,11 +8,10 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/send', function (Request $request) use ($app) {
+$app->post('/send', function (Request $request) use ($app) {
         $message = $request->get('textarea');
         $mail = $request->get('email');
         mail('robin.glauser@gmail.com', 'Feedback from '.$mail, $message);
-
         return new Response('Thank you for your feedback!', 201);
 });
 
