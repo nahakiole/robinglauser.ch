324d255397ead652773f199ad8eed947d012d36c
Nahakiole <robin.glauser@gmail.com>
2014-07-23 13:09:30 +0200

    Added form validation for contact form.

--- js/main.js
+++ js/main.js
@@ -8,10 +8,46 @@ $(function(){
         }, 1000, 'easeOutQuint');
     });
 
-    $('.contact').on('submit', function(e){
-        e.preventDefault();
-        $('.contact-form').slideUp();
-        $('.contact-feedback').slideDown();
+//    $('.contact').on('submit', function(e){
+//
+//        $('.contact-form').slideUp();
+//        $('.contact-feedback').slideDown();
+//
+//    });
 
+
+    $('.contact').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            textarea: {
+                trigger: 'blur',
+                message: '',
+                validators: {
+                    notEmpty: {
+                        message: 'I don\'t like empty messages!'
+                    },
+                    stringLength: {
+                        min: 10,
+                        message: 'I don\'t like short messages either!'
+                    }
+                }
+            },
+            email: {
+                trigger: 'blur',
+                validators: {
+                    notEmpty: {
+                        message: 'The email is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            }
+        }
     });
 });
